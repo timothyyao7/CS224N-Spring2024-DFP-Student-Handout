85c65b6e7c6e4919536209d2cd702381d0b486d3
Timothy Yao <timothyyao7@gmail.com>
2024-06-06 14:22:09 -0700

    use_bert_sts

--- snli.py
+++ snli.py
@@ -7,16 +7,19 @@ import pandas as pd
 # train dataset
 df = pd.read_parquet("data/raw/snli-train.parquet")
 df.drop(df[df.label==-1].index, inplace=True)
-df.to_csv("data/snli-train.csv")
+print(len(df))
+# df.to_csv("data/snli-train.csv")
 
 # dev dataset
 df = pd.read_parquet("data/raw/snli-dev.parquet")
 df.drop(df[df.label==-1].index, inplace=True)
-df.to_csv("data/snli-dev.csv")
+print(len(df))
+# df.to_csv("data/snli-dev.csv")
 
 # test dataset
 df = pd.read_parquet("data/raw/snli-test.parquet")
 df.drop(df[df.label==-1].index, inplace=True)
-df.to_csv("data/snli-test.csv")
+print(len(df))
+# df.to_csv("data/snli-test.csv")
 
 # after converting to csv, manually update first column header to 'id'
